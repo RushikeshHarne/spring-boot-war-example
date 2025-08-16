0c80fadf457dd9e6a5752b984d4756e605ce4957
Rushikesh Harne <161678001+RushikeshHarne@users.noreply.github.com>
2025-08-16 13:48:33 +0530

    Update HelloWorldApplication.java

--- src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/springhow/example/helloworld/HelloWorldApplication.java
@@ -23,7 +23,7 @@ public class HelloWorldApplication extends SpringBootServletInitializer {
 
     @RequestMapping("/")
     String helloWorld() {
-        return "Hello Rushikesh World!";
+        return "Hello !!!!!  Rushikesh Harne  !!!!!!! World!";
     }
 
 }
